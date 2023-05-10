485fcfdf73e143ee037154df100779f8d199dd00
laksmi santo <laksmisanto1998@gmail.com>
2023-05-10 18:02:49 +0600

    messengers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignUp from "./pages/registration";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/home";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Route>
+    )
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/DB/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDhFnC93WoZ1vZuMR4qYnDT8oxbuM8kp8",
+  authDomain: "messenger-9dabc.firebaseapp.com",
+  projectId: "messenger-9dabc",
+  storageBucket: "messenger-9dabc.appspot.com",
+  messagingSenderId: "688488169408",
+  appId: "1:688488169408:web:6b3194652220fd4f3ad225",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
