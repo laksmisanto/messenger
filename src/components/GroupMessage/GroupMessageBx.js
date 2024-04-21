6b10b9c74f335452cec03d3c5fcfbba703c6f4b8
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2024-04-21 23:37:14 +0600

    group-message-develop-start

--- src/components/GroupMessage/GroupMessageBx.js
+++ src/components/GroupMessage/GroupMessageBx.js
@@ -1,9 +1,39 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import "./style.css";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { useSelector, useDispatch } from "react-redux";
+import { activeUserChat } from "../../feature/slice/activeChatting";
 
 const GroupMessageBx = () => {
+  const [groupList, setGrouplist] = useState([]);
+  const db = getDatabase();
+  const dispatch = useDispatch();
+  const user = useSelector((users) => users.login.loggedIn);
+  useEffect(() => {
+    const starCountRef = ref(db, "groups/");
+    onValue(starCountRef, (snapshot) => {
+      let grpArr = [];
+      snapshot.forEach((item) => {
+        grpArr.push({ ...item.val(), id: item.key });
+      });
+      setGrouplist(grpArr);
+    });
+  }, []);
+  console.log(groupList);
+  const handleActiveGroup = (item) => {
+    console.log(item);
+    dispatch(
+      activeUserChat({
+        status: "group",
+        id: item.id,
+        name: item.groupname,
+        adminid: item.adminid,
+        admin: item.admin,
+      })
+    );
+  };
   return (
     <>
       <div className="msg__grouplist">
@@ -23,18 +53,23 @@ const GroupMessageBx = () => {
         </div>
 
         <div className="msg__grouplist__body">
-          <div className="msg__grouplist__wrapper">
-            <div className="msg__grouplist__img">
-              <picture>
-                <img src="./assets/avatar.png" alt="avatar" />
-              </picture>
+          {groupList.map((item, i) => (
+            <div
+              className="msg__grouplist__wrapper"
+              key={i}
+              onClick={() => handleActiveGroup(item)}
+            >
+              <div className="msg__grouplist__img">
+                <picture>
+                  <img src="./assets/avatar.png" alt="avatar" />
+                </picture>
+              </div>
+              <div className="msg__grouplist__title">
+                <h4>{item.groupname}</h4>
+                <span>{item.grouptagname}</span>
+              </div>
             </div>
-            <div className="msg__grouplist__title">
-              <h4>CSE Info</h4>
-              <span>cse education information</span>
-            </div>
-            
-          </div>
+          ))}
         </div>
       </div>
     </>
